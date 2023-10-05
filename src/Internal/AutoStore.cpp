eeea773f5e427e01d4d4546eee5f654e0e7105ca
HojdaAdelin <adelinionut297@gmail.com>
2023-10-05 13:46:29 +0300

    Auto store 3

--- src/Internal/AutoStore.cpp
+++ src/Internal/AutoStore.cpp
@@ -8,14 +8,14 @@ namespace fs = std::filesystem;
 
 void Storing::AutoStore(std::string location) {
 
-    std::string extensions[5] = {"txt", "pdf", "cpp", "doc", "pdf"};
-    std::string folder_names[5] = {"TxtFolder", "PDFFolder", "CodingFolder", "WordFolder", "PPFolder"};
+    std::string extensions[7] = {"txt", "pdf", "cpp", "docs", "pdf", "py", "java"};
+    std::string folder_names[7] = {"TxtFolder", "PDFFolder", "CppFolder", "WordFolder", "PPFolder", "PyFolder", "JavaFolder"};
 
     for (const auto& entry : fs::directory_iterator(location)) {
         if (entry.is_regular_file()) {
             std::string fileExtension = entry.path().extension().string().substr(1); // Remove the dot
 
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < 7; i++) {
                 if (fileExtension == extensions[i]) {
                     std::string destinationFolder = location + "\\" + folder_names[i];
                     fs::create_directory(destinationFolder); 
